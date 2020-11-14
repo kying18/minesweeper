9a1405930cfe2f09cb993015a397fdc1840b560b
Kylie Ying <kying@mit.edu>
2020-11-13 21:54:50 -0500

    Minesweeper implementation working

---
+++ minesweeper.py
@@ -0,0 +1,190 @@
+import random
+import re
+
+# Creating a board object to represent the minesweeper board
+# This is so that when we code up the game, we can just say "create a new board object"
+# and dig on that board, etc.
+class Board:
+    def __init__(self, dim_size, num_bombs):
+        # keep track of these parameters because we might find them helpful later on
+        self.dim_size = dim_size
+        self.num_bombs = num_bombs
+
+        # get the board
+        self.board = self.make_new_board()
+        self.assign_values_to_board()
+
+        # initialize a set to keep track of which locations we've uncovered
+        # we will put (row,col) tuples into these sets 
+        self.dug = set()
+
+    def make_new_board(self):
+        # construct a new board based on the dim size and num bombs
+        # we should construct the list of lists here (or whatever representation you prefer,
+        # but since we have a 2-D board, list of lists is most natural)
+
+        # generate a new board
+        board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        # plant the bombs
+        bombs_planted = 0
+        while bombs_planted < self.num_bombs:
+            loc = random.randint(0, self.dim_size**2 - 1) # return a random integer N such that a <= N <= b
+            row = loc // self.dim_size  # we want the number of times dim_size goes into loc to tell us what row
+            col = loc % self.dim_size  # we want the remainder to tell us what index in the row 
+            if board[row][col] == '*':
+                # this means that we've actually planted a bomb here already so we keep going in the loop
+                # without incrementing bombs_planted
+                continue 
+            board[row][col] = '*'
+            bombs_planted += 1
+
+        return board
+
+    def assign_values_to_board(self):
+        # now that we have the bombs planted, let's assign a number 0-8 for all the empty spaces, which
+        # represents how many neighboring bombs there are. we can precompute these and it'll save us some
+        # effort checking what's around the board later on :)
+        for r in range(self.dim_size):
+            for c in range(self.dim_size):
+                if self.board[r][c] == '*': # if this is already a bomb, we want to skip over
+                    continue
+                self.board[r][c] = self.get_num_neighboring_bombs(r, c)
+
+    def get_num_neighboring_bombs(self, row, col):
+        # let's iterate through each of the neighboring positions and sum number of bombs
+        # top left: (row-1, col-1)
+        # top middle: (row-1, col)
+        # top right: (row-1, col+1)
+        # left: (row, col-1)
+        # right: (row, col+1)
+        # bottom left: (row+1, col-1)
+        # bottom middle: (row+1, col)
+        # bottom right: (row+1, col+1)
+
+        # ps we need to make sure we don't go out of bounds!!
+
+        num_neighboring_bombs = 0  # we will use this variable to keep track of how many we see
+        for r in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
+            for c in range(max(0, col-1), min(self.dim_size-1, col+1)+1):
+                if r == row and c == col:
+                    # we don't want to check the location itself
+                    continue
+                if self.board[r][c] == '*':
+                    num_neighboring_bombs += 1
+        return num_neighboring_bombs
+
+    def dig(self, row, col):
+        # dig at that location!
+        # return True if successful dig, False if bomb dug
+
+        # a couple of scenarios:
+        # hit a bomb -> game over
+        # dig at a location with neighboring bombs -> finish dig
+        # dig at a location with no neighboring bombs -> recursively dig neighbors!
+
+        self.dug.add((row,col)) # add to set to keep track that we dug there
+
+        if self.board[row][col] == '*':
+            return False
+        elif self.board[row][col] > 0:
+            return True
+
+        # self.board[row][col] == 0
+        for r in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
+            for c in range(max(0, col-1), min(self.dim_size-1, col+1)+1):
+                if (r,c) in self.dug: 
+                    # we don't want to dig where we already dug!!
+                    continue
+                self.dig(r, c)
+
+        # we should never recursively dig and accidentally hit a bomb
+        return True
+
+    def __str__(self):
+        # return a string that shows the board to the player
+
+        # first let's create a new array that represents what the user would see
+        # we will use numbers and spaces
+        visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        # let's step through all the possible coordinates and assign a value
+        for row in range(self.dim_size):
+            for col in range(self.dim_size):
+                if (row,col) in self.dug:
+                    visible_board[row][col] = str(self.board[row][col])
+                else:
+                    visible_board[row][col] = ' '
+
+        # now let's put this together in a string
+        # string_rep = "\n".join(map(str, visible_board))
+        # return string_rep
+        string_rep = ''
+        # get max column widths for printing
+        widths = []
+        for idx in range(self.dim_size):
+            columns = map(lambda x: x[idx], visible_board)
+            widths.append(
+                len(
+                    max(columns, key = len)
+                )
+            )
+
+        # print the csv strings
+        indices = [i for i in range(self.dim_size)]
+        indices_row = '   '
+        cells = []
+        for idx, col in enumerate(indices):
+            format = '%-' + str(widths[idx]) + "s"
+            cells.append(format % (col))
+        indices_row += '  '.join(cells)
+        indices_row += '  \n'
+        
+        for i in range(len(visible_board)):
+            row = visible_board[i]
+            string_rep += f'{i} |'
+            cells = []
+            for idx, col in enumerate(row):
+                format = '%-' + str(widths[idx]) + "s"
+                cells.append(format % (col))
+            string_rep += ' |'.join(cells)
+            string_rep += ' |\n'
+
+        str_len = int(len(string_rep) / self.dim_size)
+        string_rep = indices_row + '   ' + '-'*str_len + '\n' + string_rep + '   ' + '-'*str_len
+
+        return string_rep
+
+def play(dim_size=10, num_bombs=10):
+    # Step 1: create the board and plant the bombs
+    board = Board(dim_size, num_bombs)
+
+    # Step 2: show the user the board and ask for where they want to dig
+    # Step 3a: if the location is a bomb, then show game over message
+    # Step 3b: if the location is not a bomb, dig recursively until one of the squares is next to a bomb
+    # Step 4: repeat steps 2 and 3a/b until there are no more places to dig, then show victory
+    safe = True
+
+    while len(board.dug) < board.dim_size**2 - num_bombs:
+        print(board)
+        user_input = re.split(',(\\s)*', input("Where would you like to dig? Input as row, col: "))
+        row, col = int(user_input[0]), int(user_input[-1])
+        if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
+            print("Invalid location. Try again.")
+            continue
+        
+        safe = board.dig(row, col)
+        if not safe:  # if we've dug a bomb, it's game over and we need to break out of the loop
+            break
+
+    # now there are two ways to end the loop, either we met the while loop condition or safe is now false
+    # lets check both of these by just checking safe
+    if safe:
+        print("CONGRATULATIONS!!! YOU ARE VICTORIOUS!")
+    else:
+        print("SORRY GAME OVER :(")
+        # lets reveal the whole board!
+        board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
+        print(board)
+
+if __name__=='__main__':
+    play(10, 10)
+    
